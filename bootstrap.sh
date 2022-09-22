c973e197cfe045894dc43b9cdbb642ab36c364ed
Lucas Draney <107153866+ldraney@users.noreply.github.com>
2022-09-21 22:55:04 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,12 @@ apt-get upgrade -y
 eval "$(ssh-agent -s)"
 ssh-add /home/ldraney/.ssh/id_ed25519
 
-mkdir  /home/ldraney/github
-cd /home/ldraney/github
+#mkdir  /home/ldraney/github
+#cd /home/ldraney/github
 
-git clone git@github.com:ldraney/ansible_desktop_setup.git
-git clone git@github.com:ldraney/dotfiles.git
-git clone git@github.com:ldraney/sensitive.git
+#git clone git@github.com:ldraney/ansible_desktop_setup.git
+#git clone git@github.com:ldraney/dotfiles.git
+#git clone git@github.com:ldraney/sensitive.git
 
 ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519 --accept-host-key --ask-become-pass
-sudo reboot now
+#sudo reboot now
