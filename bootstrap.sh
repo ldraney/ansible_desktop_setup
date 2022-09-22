a710d451e7f94071192d8fd8d5d40e7c41122db6
ldraney <draneylucas@gmail.com>
2022-09-21 21:58:25 -0600

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 mkdir  ~/github
 cd ~/github
-git@github.com:ldraney/ansible_desktop_setup.git
+git clone git@github.com:ldraney/ansible_desktop_setup.git
 git clone git@github.com:ldraney/dotfiles.git
-git@github.com:ldraney/sensitive.git
-sudo ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file ~/.ssh/id_ed25519 --accept-host-key --ask-become-pass --check
+git clone git@github.com:ldraney/sensitive.git
+sudo ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file ~/.ssh/id_ed25519 --accept-host-key --ask-become-pass
+sudo reboot now
