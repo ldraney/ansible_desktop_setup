77394574f3a2b297044c3e1be69879fc0729099c
ldraney <draneylucas@gmail.com>
2022-09-23 01:16:13 -0600

    juss checkin in

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,5 +24,7 @@ ssh-add /home/ldraney/.ssh/id_ed25519
 #git clone git@github.com:ldraney/dotfiles.git
 #git clone git@github.com:ldraney/sensitive.git
 
-ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519 --accept-host-key --ask-become-pass
+#use this for developing my ansible script
+#ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519 --accept-host-key --ask-become-pass
+sudo ansible-playbook local.yml
 sudo reboot now
