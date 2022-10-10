5a60bc4f29dd9f9a5f1aa27642f87c83ee5bc9b5
Lucas Draney <107153866+ldraney@users.noreply.github.com>
2022-10-10 12:04:45 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ set -e    #I need to restart computer after this command for vm shared folder to
 # this gives an error
 echo vmhgfs-fuse /mnt/hgfs  fuse defaults,allow_other   0   0 | sudo tee -a /etc/fstab
 sudo mount -a
-mkdir -p /home/ldraney/.ssh
-cp -r /mnt/hgfs/VMShare/ssh/* /home/ldraney/.ssh/
-chmod 400 /home/ldraney/.ssh/*
-chown -R ldraney:ldraney /home/ldraney/.ssh
+mkdir -p $HOME/.ssh
+cp -r /mnt/hgfs/VMShare/ssh/* $HOME/.ssh/
+chmod 400 $HOME/.ssh/*
+chown -R ldraney:ldraney $HOME/.ssh
 
 #rest of the install
 apt-get update
@@ -15,16 +15,16 @@ apt-get upgrade -y
 sudo apt-get install -y git ansible
 
 eval "$(ssh-agent -s)"
-ssh-add /home/ldraney/.ssh/id_ed25519
+ssh-add $HOME/.ssh/id_ed25519
 
-#mkdir  /home/ldraney/github
-#cd /home/ldraney/github
+#mkdir $HOME/github
+#cd $HOME/github
 
 #git clone git@github.com:ldraney/ansible_desktop_setup.git
 #git clone git@github.com:ldraney/dotfiles.git
 #git clone git@github.com:ldraney/sensitive.git
 
 #use this for developing my ansible script
-#ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519 --accept-host-key --ask-become-pass
+#ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file $HOME/.ssh/id_ed25519 --accept-host-key --ask-become-pass
 sudo ansible-playbook local.yml
 sudo reboot now
